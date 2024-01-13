3058eba2c9403ac30aa295b11b9b4b2091b89cc1
dennismoha <dennismoha2@gmail.com>
2024-01-13 13:09:17 +0300

    initialized and setup the server

---
+++ backend/src/setupServer.ts
@@ -0,0 +1,82 @@
+import {
+  Application,
+  json,
+  urlencoded,
+  Response,
+  Request,
+  NextFunction,
+} from "express";
+import http  from "http";
+
+import cors from "cors";
+import helmet from "helmet";
+import hpp from "hpp";
+import cookieSession from "cookie-session";
+import HTTP_STATUS from "http-status-codes";
+import "express-async-errors";
+import compression from "compression";
+
+const SERVER_PORT = 8000;
+
+export class ChattyServer {
+  private app: Application;
+
+  constructor(app: Application) {
+    this.app = app;
+  }
+
+  public Start(): void {
+    this.securityMiddleware(this.app);
+    this.standardMiddleware(this.app);
+    this.routeMiddleware(this.app);
+    this.globalErrorHandler(this.app);
+    this.startServer(this.app);
+  }
+
+  private securityMiddleware(app: Application): void {
+    app.use(
+      cookieSession({
+        name: "session",
+        keys: ["test1", "test2"],
+        maxAge: 24 * 7 * 3600000,
+        secure: false,
+      })
+    );
+    app.use(hpp());
+    app.use(helmet());
+    app.use(
+      cors({
+        origin: "*",
+        credentials: true,
+        optionsSuccessStatus: 200,
+        methods: ["GET", "PUT", "POST", "DELETE", "OPTIONS"],
+      })
+    );
+  }
+
+  private standardMiddleware(app: Application): void {
+    app.use(compression());
+    app.use(json({ limit: "50mb" }));
+    app.use(urlencoded({ extended: true, limit: "50mb" }));
+  }
+  private routeMiddleware(app: Application): void {}
+  private globalErrorHandler(app: Application): void {}
+
+  // creating http server
+  private async startServer(app: Application): Promise<void> {
+    try {
+        const httpServer: http.Server = new http.Server(app);
+        this.startHttpServer(httpServer)
+    } catch (error) {
+        console.log('error is ', error)
+    }
+  }
+  private createSocketIO(httpServer: http.Server): void {}
+
+  // calling the listen method
+  private startHttpServer(httpServer: http.Server): void {
+    httpServer.listen(SERVER_PORT, () => {
+      console.log("server running ", SERVER_PORT);
+    });
+  }
+}
