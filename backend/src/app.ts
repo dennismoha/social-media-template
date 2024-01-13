0201038cb3d97ec310890a0e64d4d78a81424237
dennismoha <dennismoha2@gmail.com>
2024-01-13 15:07:05 +0300

    added the database setup logic.  installed the .env

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,9 +1,11 @@
 import express, {Express} from 'express';
 
 import { ChattyServer } from './setupServer';
+import setupDatabase from 'src/setupDatabase';
 
 class Application {
     public initialize(): void{
+        setupDatabase()
         const app: Express = express();
         const server:ChattyServer = new ChattyServer(app)
         server.Start()
