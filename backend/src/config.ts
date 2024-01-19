f1fc5a3011dd315fcc5de6dd6aebe239a1404cbd
dennismoha <dennismoha2@gmail.com>
2024-01-19 20:55:06 +0300

    finished setting up mongodb

---
+++ backend/src/config.ts
@@ -0,0 +1,38 @@
+import dotenv from "dotenv";
+import { MONGO_DATABASE_URL } from "./constants";
+
+dotenv.config({});
+
+class Config {
+  public DATABASE_URL: string | undefined;
+  public DATABASE_USERNAME: string | undefined;
+  public DATABASE_PASSWORD: string | undefined;
+  public JWT_TOKEN: string | undefined;
+  public NODE_ENV: string | undefined;
+  public SECRET_COOKIE_KEY_ONE: string | undefined;
+  public SECRET_COOKIE_KEY_TWO: string | undefined;
+  public CLIENT_URL: string | undefined;
+
+  private readonly DEFAULT_DATABASE_URL = MONGO_DATABASE_URL;
+
+  constructor() {
+    this.DATABASE_USERNAME = process.env.DATABASE_USERNAME || "";
+    this.DATABASE_PASSWORD = process.env.DATABASE_PASSWORD || undefined;
+    this.DATABASE_URL = MONGO_DATABASE_URL;
+    this.JWT_TOKEN = process.env.JWT_TOKEN || "BLABLABLA";
+    this.NODE_ENV = process.env.NODE_ENV || "";
+    this.SECRET_COOKIE_KEY_ONE = process.env.SECRETE_COOKIE_KEY_ONE || "";
+    this.SECRET_COOKIE_KEY_TWO = process.env.SECRETE_COOKIE_KEY_TWO || "";
+    this.CLIENT_URL = process.env.CLIENT_URL || "";
+  }
+
+  public validateConfig(): void{
+    for(const [key, value]  of Object.entries(this)){
+        if(value === undefined){
+            throw new Error(`${key} configuration is undefined`)
+        }
+    }
+  }
+}
+
+export const config: Config = new Config();
---
+++ backend/src/constants.ts
@@ -0,0 +1,3 @@
+export const MONGO_DATABASE_URL = `mongodb+srv://georgekinoti254:${encodeURIComponent(
+  "@$$Mon254"
+)}@cluster0.v5pwujv.mongodb.net/?retryWrites=true&w=majority`;
