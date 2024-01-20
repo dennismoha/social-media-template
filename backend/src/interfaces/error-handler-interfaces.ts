566ddba5fed05d9d244e0594c7c8986ad83c04b8
dennismoha <dennismoha2@gmail.com>
2024-01-20 15:03:48 +0300

    added all prettier , eslint and editorConfig Configuration logic

--- backend/src/interfaces/error-handler-interfaces.ts
+++ backend/src/interfaces/error-handler-interfaces.ts
@@ -1,12 +1,12 @@
 export interface IErrorResponse {
-    message: string;
-    statusCode: number;
-    status: string;
-    serializeErrors(): IError;
+  message: string;
+  statusCode: number;
+  status: string;
+  serializeErrors(): IError;
 }
 
 export interface IError {
-    message: string;
-    statusCode: number;
-    status: string
+  message: string;
+  statusCode: number;
+  status: string;
 }
