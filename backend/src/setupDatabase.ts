882b9509055bf617a86d9143ef8879f0e1773f2c
dennismoha <dennismoha2@gmail.com>
2024-01-22 16:23:53 +0300

    added redis configuration. redis is also working well as tested

--- backend/src/setupDatabase.ts
+++ backend/src/setupDatabase.ts
@@ -1,8 +1,10 @@
 import mongoose from 'mongoose';
 import Logger from 'bunyan';
 
+
 import { MONGO_DATABASE_URL } from './constants';
 import { config } from '@src/config';
+import { redisConnection } from '@src/shared/services/redis/redis.connection';
 
 const log: Logger = config.createLogger('databaseSetup');
 
@@ -13,6 +15,7 @@ export default () => {
 
       .then(() => {
         log.info('successfully connected to db');
+        redisConnection.connect();
       })
       .catch((error) => {
         log.error('error is ', error);
