43abbd3988f4e0cbc3704e5d78b5f2f4974e4061
dennismoha <dennismoha2@gmail.com>
2024-01-30 14:39:11 +0300

    added redis method for getting a particular user hash

--- backend/src/shared/globals/helpers/helpers.ts
+++ backend/src/shared/globals/helpers/helpers.ts
@@ -21,4 +21,14 @@ export class Helpers {
     }
     return parseInt(result, 10);
   }
+
+  // this will return the stringified user data in redis
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  static parseJson(prop: string): any {
+    try {
+      JSON.parse(prop);
+    } catch (error) {
+      return prop;
+    }
+  }
 }
