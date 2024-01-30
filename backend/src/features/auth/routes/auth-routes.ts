b6d0b448366a9ef07d4d4f970ad3d412ada31cb2
dennismoha <dennismoha2@gmail.com>
2024-01-30 14:13:21 +0300

    added logout functionality

--- backend/src/features/auth/routes/auth-routes.ts
+++ backend/src/features/auth/routes/auth-routes.ts
@@ -1,4 +1,5 @@
 import { SignIn } from '@src/features/auth/controller/signin';
+import { SignOut } from '@src/features/auth/controller/signout';
 import { Signup } from '@src/features/auth/controller/signup';
 import express, { Router } from 'express';
 
@@ -13,6 +14,12 @@ class AuthRoutes {
   public routes(): Router {
     this.router.post('/signup', Signup.prototype.create);
     this.router.post('/login', SignIn.prototype.read);
+    return  this.router;
+  }
+
+  public signoutRoute(): Router {
+    this.router.get('/logout', SignOut.prototype.update);
+
     return  this.router;
   }
 }
