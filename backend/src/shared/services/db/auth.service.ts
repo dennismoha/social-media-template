1005b22244ec107370a63e0ab06727458c6c8340
dennismoha <dennismoha2@gmail.com>
2024-01-30 14:00:20 +0300

    user can login

--- backend/src/shared/services/db/auth.service.ts
+++ backend/src/shared/services/db/auth.service.ts
@@ -1,12 +1,20 @@
 import { AuthModel } from '@src/features/auth/models/auth.schema';
 import { IAuthDocument } from '@src/interfaces/auth.interface';
 import { Helpers } from '@src/shared/globals/helpers/helpers';
+import Logger from 'bunyan';
+
+import { config } from '@src/config';
+
+
+
+const log: Logger = config.createLogger('singup');
 
 class Authservice {
   // this methods creates a user to the db:
   // we use it in our auth.worker.ts
 
   public async createAuthUser(data: IAuthDocument): Promise<void>{
+    log.error('create auth user ', data);
     await AuthModel.create(data);
   }
 
@@ -18,6 +26,8 @@ class Authservice {
   return user;
   }
   public async getAuthUserByUsername(username: string): Promise<IAuthDocument> {
+    log.error('username in get auth by username ', username);
+    log.error('username:::: in get auth by username ', Helpers.firstLetterToUpperCase(username) );
     const user: IAuthDocument = (await AuthModel.findOne({ username: Helpers.firstLetterToUpperCase(username) }).exec()) as IAuthDocument;
     return user;
   }
