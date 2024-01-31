f54675e89754b32cf78645f4b540ab41e6e51a9a
dennismoha <dennismoha2@gmail.com>
2024-01-31 11:40:12 +0300

    added current user logic. added logic to fetch user from  redis first and from the db  if redis is down or user data not cached

---
+++ backend/src/shared/globals/helpers/auth-middlewares.ts
@@ -0,0 +1,35 @@
+import { config } from '@src/config';
+import { AuthPayload } from '@src/interfaces/auth.interface';
+import { NotAuthorizedError } from '@src/shared/globals/helpers/error-handler';
+import { Request, Response, NextFunction } from 'express';
+import JWT from 'jsonwebtoken';
+
+export class AuthMiddleware {
+  // verify the user and check if token is valid
+
+  public verifyUser(req: Request, _res: Response, next: NextFunction): void {
+    if (!req.session?.jwt) {
+      throw new NotAuthorizedError(' Token invalid');
+    }
+
+    try {
+      const payload: AuthPayload = JWT.verify(req.session?.jwt, config.JWT_TOKEN) as AuthPayload;
+      console.log('req current user is ', payload);
+      req.currentUser = payload;
+    } catch (error) {
+      throw new NotAuthorizedError('token invalid. please login again');
+    }
+    next();
+  }
+
+  // check if user is authenticated
+
+  public checkAuthentication(req: Request, _res: Response, next: NextFunction): void {
+    if (!req.currentUser) {
+      throw new NotAuthorizedError('You are not authenticated');
+    }
+    next();
+  }
+}
+
+export const authMiddleware: AuthMiddleware = new AuthMiddleware();
