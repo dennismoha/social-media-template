2c4a53a7548940dc84a72ad5bbeae5f58348b38c
dennismoha <dennismoha2@gmail.com>
2024-02-02 15:30:01 +0300

    password reset logic in the controller is working created the password forgot-password route also working created the password reset-password route also working

--- backend/src/shared/services/db/auth.service.ts
+++ backend/src/shared/services/db/auth.service.ts
@@ -5,38 +5,39 @@ import Logger from 'bunyan';
 
 import { config } from '@src/config';
 
-
-
 const log: Logger = config.createLogger('singup');
 
 class Authservice {
   // this methods creates a user to the db:
   // we use it in our auth.worker.ts
 
-  public async createAuthUser(data: IAuthDocument): Promise<void>{
+  public async createAuthUser(data: IAuthDocument): Promise<void> {
     log.error('create auth user ', data);
     await AuthModel.create(data);
   }
 
   // password token update;
 
-  public async updatePasswordToken(authId: string, token: string, tokenExpiration: number): Promise<void>{
-    await AuthModel.updateOne({_id: authId}, {
-      passwordResetToken: token,
-      passwordResetExpires: tokenExpiration
-    });
+  public async updatePasswordToken(authId: string, token: string, tokenExpiration: number): Promise<void> {
+    await AuthModel.updateOne(
+      { _id: authId },
+      {
+        passwordResetToken: token,
+        passwordResetExpires: tokenExpiration
+      }
+    );
   }
 
   public async getUserByNameOrEmail(username: string, email: string): Promise<IAuthDocument> {
     const query = {
       $or: [{ username: Helpers.firstLetterToUpperCase(username) }, { email: Helpers.lowerCase(email) }]
     };
-  const user: IAuthDocument  = await AuthModel.findOne(query).exec() as IAuthDocument;
-  return user;
+    const user: IAuthDocument = (await AuthModel.findOne(query).exec()) as IAuthDocument;
+    return user;
   }
   public async getAuthUserByUsername(username: string): Promise<IAuthDocument> {
     log.error('username in get auth by username ', username);
-    log.error('username:::: in get auth by username ', Helpers.firstLetterToUpperCase(username) );
+    log.error('username:::: in get auth by username ', Helpers.firstLetterToUpperCase(username));
     const user: IAuthDocument = (await AuthModel.findOne({ username: Helpers.firstLetterToUpperCase(username) }).exec()) as IAuthDocument;
     return user;
   }
@@ -46,7 +47,13 @@ class Authservice {
     return user;
   }
 
-
+  public async getAuthUserByPasswordToken(token: string): Promise<IAuthDocument> {
+    const user: IAuthDocument = (await AuthModel.findOne({
+      passwordResetToken: token,
+      passwordResetExpires: { $gt: Date.now() }
+    }).exec()) as IAuthDocument;
+    return user;
+  }
 }
 
 export const authservice: Authservice = new Authservice();
