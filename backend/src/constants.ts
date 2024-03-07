62f80f0dcbbd5241e82cac47300ac26e9a708aa1
dennismoha <dennismoha2@gmail.com>
2024-03-07 13:19:26 +0300

    added code to fetch and posting to reaction-cache added code to fetch and posting to reaction-db added reaction workers and queues logic

--- backend/src/constants.ts
+++ backend/src/constants.ts
@@ -4,11 +4,15 @@ export const MONGO_DATABASE_URL = `mongodb+srv://georgekinoti254:${encodeURIComp
 
 export const BASE_PATH = '/api/v1';
 
+// JOB NAMES
 export const ADD_AUTH_USER_TO_JOB = 'addAuthUserToJob';
 export const ADD_USER_TO_JOB = 'addUserToJob';
 export const ADD_USER_POST_TO_JOB = 'addUserPostToJob';
 export const DELETE_USER_POST_TO_JOB = 'deleteUserPostToJob';
 export const EDIT_USER_POST_TO_JOB = 'editUserPostToJob';
+
+export const ADD_REACTION_TO_DB_JOB = 'addUserReactionToDbJob';
+export const REMOVE_REACTION_FROM_DB_JOB = 'removeUserReactionFromDbJob';
 
 // EMAIL JOB NAMES
 
