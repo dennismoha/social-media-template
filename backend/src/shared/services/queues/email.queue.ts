2da18e8649bf79012639e9a749ae20a42225ae52
dennismoha <dennismoha2@gmail.com>
2024-03-19 09:28:57 +0300

    Added comment notifications; added email to receive comment notifications added email template for comment notifications

--- backend/src/shared/services/queues/email.queue.ts
+++ backend/src/shared/services/queues/email.queue.ts
@@ -1,4 +1,4 @@
-import { FORGOT_PASSWORD } from '@src/constants';
+import { COMMENT_EMAIL, FORGOT_PASSWORD } from '@src/constants';
 import { IEmailJob } from '@src/features/user/interfaces/user.interface';
 import { BaseQueue } from '@src/shared/services/queues/base.queue';
 import { emailWorker } from '@src/shared/workers/email.worker';
@@ -8,6 +8,7 @@ class EmailQueue extends BaseQueue {
   constructor(){
     super('emails');
     this.processJob( FORGOT_PASSWORD, 5, emailWorker.addNotificationEmail);
+    this.processJob( COMMENT_EMAIL, 5, emailWorker.addNotificationEmail);
   }
 
   public AddEmailJob(name: string, data: IEmailJob): void {
