c9e8fc39695a74f62e10be1953572d45dcba966a
dennismoha <dennismoha2@gmail.com>
2024-03-20 13:50:04 +0300

    added code to fetch all images either for the logged in user or for any other user

--- backend/src/features/images/routes/imageRoute.ts
+++ backend/src/features/images/routes/imageRoute.ts
@@ -1,5 +1,6 @@
 import { Add } from '@src/features/images/controllers/add-image';
 import { Delete } from '@src/features/images/controllers/delete-image';
+import { Get } from '@src/features/images/controllers/get-images';
 import { authMiddleware } from '@src/shared/globals/helpers/auth-middlewares';
 import express, { Router } from 'express';
 
@@ -12,7 +13,7 @@ class ImageRoutes {
   }
 
   public routes(): Router {
-    // this.router.get('/images/:userId', authMiddleware.checkAuthentication, Get.prototype.images);
+    this.router.get('/images/:userId', authMiddleware.checkAuthentication, Get.prototype.images);
     this.router.post('/images/profile', authMiddleware.checkAuthentication, Add.prototype.profileImage);
     this.router.post('/images/background', authMiddleware.checkAuthentication, Add.prototype.backgroundImage);
     this.router.delete('/images/:imageId', authMiddleware.checkAuthentication, Delete.prototype.image);
