6a12f8ab42aab78eba232e1a007461da57aa178b
dennismoha <dennismoha2@gmail.com>
2024-03-28 11:48:41 +0300

    added video upload method to cloudinary upload file

--- backend/src/shared/globals/helpers/cloudinary-upload.ts
+++ backend/src/shared/globals/helpers/cloudinary-upload.ts
@@ -19,3 +19,26 @@ export function uploads(
     );
   });
 }
+
+//video upload
+
+export function videoUpload(
+  file: string,
+  public_id?: string,
+  overwrite?: boolean,
+  invalidate?: boolean
+): Promise<UploadApiErrorResponse | UploadApiResponse | undefined> {
+  return new Promise((resolve) => {
+    cloudinary.v2.uploader.upload(
+      file,
+
+      { resource_type: 'video', chunk_size: 50000, public_id, overwrite, invalidate }, // 50000 for chunksize is in mb
+      (error: UploadApiErrorResponse | undefined, result: UploadApiResponse | undefined) => {
+        if (error) {
+          resolve(error);
+        }
+        resolve(result);
+      }
+    );
+  });
+}
