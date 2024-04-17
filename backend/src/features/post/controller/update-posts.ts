91fdf28164a58d97790bc9c679a10322bc97e47b
dennismoha <dennismoha2@gmail.com>
2024-04-17 09:10:43 +0300

    added the fetching and creating posts with videos functionality

--- backend/src/features/post/controller/update-posts.ts
+++ backend/src/features/post/controller/update-posts.ts
@@ -1,14 +1,14 @@
 import HTTP_STATUS from 'http-status-codes';
 import { IPostDocument } from '@src/features/post/interfaces/post.interface';
-import { postSchema, postWithImageSchema } from '@src/features/post/schemes/post.schemes';
+import { postSchema, postWithImageSchema, postWithVideoSchema } from '@src/features/post/schemes/post.schemes';
 import { joiValidation } from '@src/shared/globals/decorators/joi-validation-decorators';
 import { Request, Response } from 'express';
 import { PostCache } from '@src/shared/services/redis/post.cache';
 import { SocketIOPostObject } from '@src/shared/sockets/posts';
 import { postQueue } from '@src/shared/services/queues/post.queue';
-import {  ADD_IMAGE_TO_DB_JOB, EDIT_USER_POST_TO_JOB } from '@src/constants';
+import { ADD_IMAGE_TO_DB_JOB, EDIT_USER_POST_TO_JOB } from '@src/constants';
 import { UploadApiResponse } from 'cloudinary';
-import { uploads } from '@src/shared/globals/helpers/cloudinary-upload';
+import { uploads, videoUpload } from '@src/shared/globals/helpers/cloudinary-upload';
 import { BadRequestError } from '@src/shared/globals/helpers/error-handler';
 import { imageQueue } from '@src/shared/services/queues/image.queue';
 
@@ -18,7 +18,7 @@ export class UpdatePost {
   // handles updating user post without an image
   @joiValidation(postSchema)
   public async post(req: Request, res: Response): Promise<void> {
-    const { post, bgColor, feelings, privacy, gifUrl, imgVersion, imgId, profilePicture } = req.body;
+    const { post, bgColor, feelings, privacy, gifUrl, imgVersion, imgId, profilePicture, videoId, videoVersion } = req.body;
     const { postId } = req.params;
 
     const UpdatePost: IPostDocument = {
@@ -29,7 +29,9 @@ export class UpdatePost {
       gifUrl,
       imgVersion,
       imgId,
-      profilePicture
+      profilePicture,
+      videoId,
+      videoVersion
     } as IPostDocument;
 
     const postUpdatedInCache: IPostDocument = await postCache.updatePostInCache(postId, UpdatePost);
@@ -43,6 +45,10 @@ export class UpdatePost {
   // Handles updating a  user post that contains an image
 
   /*
+
+
+
+  `
     Now three scenarios here:
       The user wants to update a post with an image but:
         1) wants to update the post test but doesn't want to change an image
@@ -58,7 +64,7 @@ export class UpdatePost {
     if (imgId && imgVersion) {
       UpdatePost.prototype.UpdatePostUtility(req);
     } else {
-      const result: UploadApiResponse = await UpdatePost.prototype.UpdatePostWithNewImage(req);
+      const result: UploadApiResponse = await UpdatePost.prototype.addFileToExistingPost(req);
 
       if (!result.public_id) {
         throw new BadRequestError(result.message);
@@ -68,8 +74,25 @@ export class UpdatePost {
     res.status(HTTP_STATUS.CREATED).json({ message: 'posts with image updated successfull ' });
   }
 
+  // update post with video
+  @joiValidation(postWithVideoSchema)
+  public async updatePostWithVideo(req: Request, res: Response): Promise<void> {
+    const { videoId, videoVersion } = req.body;
+    if (videoId && videoVersion) {
+      UpdatePost.prototype.UpdatePostUtility(req);
+    } else {
+      const result: UploadApiResponse = await UpdatePost.prototype.addFileToExistingPost(req);
+      if (!result.public_id) {
+        throw new BadRequestError(result.message);
+      }
+    }
+    res.status(HTTP_STATUS.OK).json({ message: 'Post with video updated successfully' });
+  }
+
+  // updare post
+
   public async UpdatePostUtility(req: Request): Promise<void> {
-    const { post, bgColor, feelings, privacy, gifUrl, imgVersion, imgId, profilePicture } = req.body;
+    const { post, bgColor, feelings, privacy, gifUrl, imgVersion, imgId, profilePicture, videoId, videoVersion } = req.body;
     const { postId } = req.params;
 
     const UpdatePost: IPostDocument = {
@@ -78,9 +101,11 @@ export class UpdatePost {
       feelings,
       privacy,
       gifUrl,
-      imgVersion,
-      imgId,
-      profilePicture
+      imgVersion: imgVersion ? imgVersion : '',
+      imgId: imgId ? imgId : '',
+      profilePicture,
+      videoId: videoId ? videoId : '',
+      videoVersion: videoVersion ? videoVersion : ''
     } as IPostDocument;
 
     const postUpdatedInCache: IPostDocument = await postCache.updatePostInCache(postId, UpdatePost);
@@ -93,8 +118,8 @@ export class UpdatePost {
 
   // Add image to existing post
   // if after creating a post and the user want to add a new image, this logic will take care of that
-  public async UpdatePostWithNewImage(req: Request): Promise<UploadApiResponse> {
-    const { post, bgColor, feelings, privacy, gifUrl, profilePicture, image } = req.body;
+  public async addFileToExistingPost(req: Request): Promise<UploadApiResponse> {
+    const { post, bgColor, feelings, privacy, gifUrl, profilePicture, image, video } = req.body;
     const { postId } = req.params;
 
     /* Here unlike the signup we don't pass three params on the uploads since here we allow cloudinary to generate
@@ -103,7 +128,9 @@ export class UpdatePost {
       for us.
 
       */
-    const result: UploadApiResponse = (await uploads(image)) as UploadApiResponse;
+    const result: UploadApiResponse = image
+      ? ((await uploads(image)) as UploadApiResponse)
+      : ((await videoUpload(video)) as UploadApiResponse);
 
     if (!result?.public_id) {
       throw new BadRequestError(result.message);
@@ -116,8 +143,10 @@ export class UpdatePost {
       privacy,
       gifUrl,
       profilePicture,
-      imgVersion: result.version.toString(),
-      imgId: result.public_id
+      imgVersion: image ? result.version.toString() : '',
+      imgId: image ? result.public_id : '',
+      videoVersion: image ? result.version.toString() : '',
+      videoId: image ? result.public_id : ''
     } as IPostDocument;
 
     const postUpdatedInCache: IPostDocument = await postCache.updatePostInCache(postId, UpdatePost);
@@ -125,12 +154,16 @@ export class UpdatePost {
     // this can be either before saving to cache or after.
     SocketIOPostObject.emit('updated post', postUpdatedInCache, 'post');
     postQueue.AddPostJob(EDIT_USER_POST_TO_JOB, { key: postId, value: UpdatePost });
-    // call image queue to add image to mongodb database
-    imageQueue.addImageJob(ADD_IMAGE_TO_DB_JOB, {
-      key: `${req.currentUser!.userId}`,
-      imgId: result.public_id,
-      imgVersion: result.version.toString()
-    });
+
+    if (image) {
+      // call image queue to add image to mongodb database
+      imageQueue.addImageJob(ADD_IMAGE_TO_DB_JOB, {
+        key: `${req.currentUser!.userId}`,
+        imgId: result.public_id,
+        imgVersion: result.version.toString()
+      });
+    }
+
     return result;
   }
 }
